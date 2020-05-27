77d3548e283d3f8ceee78ce52b0729bc9c1cabd2
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-27 15:46:13 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}.\n"
+end
+
+def introduction_with_language("Dan," "Ember.js")
+  puts "Hi, my name is #{name} and I am learning to program in #Ember.js."
+end
